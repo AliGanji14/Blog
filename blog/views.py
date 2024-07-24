75def665c75e3e2d2651fb21df367489a3e55a1d
Ali Ganji <aliganji142879@gmail.com>
2024-07-24 16:44:22 +0330

    add model comment

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -16,7 +17,9 @@ class PostListView(ListView):
 
 def post_detail(request, id):
     post = get_object_or_404(Post, pk=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -39,3 +42,15 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post_share.html', {'form': form, 'post': post, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/comment.html', {'post': post, 'form': form, 'comment': comment})
